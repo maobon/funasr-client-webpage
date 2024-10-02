fc09f2e5695e8edbeeae8b13a9be24b28673b50f
maobonhaha <maobonhaha@gmail.com>
2024-10-02 12:02:54 +0800

    first commit

---
+++ static/homepage.js
@@ -0,0 +1,7 @@
+console.log('nimabi')
+
+const btn_start_record = document.getElementById('btn_start_record');
+btn_start_record.addEventListener('click', () => {
+
+    window.location.href = 'index.html';
+})
